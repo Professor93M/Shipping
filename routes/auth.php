a1dc0c4f825d81eec5890d9ec338510fad39c5fb
unknown <professor404@gmail.com>
2022-03-20 20:50:50 +0300

    Fff

--- routes/auth.php
+++ routes/auth.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+                ->name('register');
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
@@ -14,11 +19,4 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
-});
-
-Route::middleware(['auth', 'guest'])->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-    ->name('register');
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
 });
